c14bff2673e4693497c578e12f4f93ae775890a6
Mohamed Ali Ben Ismail <leonardolucciano35@gmail.com>
2018-09-16 02:05:42 +0200

    Wrapping the code up inside a function

--- atm.py
+++ atm.py
@@ -1,19 +1,32 @@
 
 balance = 500
-request = 650
+request = 360
 
-# Allowed papers and piece of money in Tunisia
-allowed_papers = [100, 50, 20, 10, 5, 2, 1]
+def withdraw(balance, request):
+    try:
+        if request > balance:
+            raise ValueError("Invalid Amount")
 
-for i in range(len(allowed_papers)):
-    while request >= allowed_papers[i]:
-        print("Give -> %3i DNT" %(allowed_papers[i]))
-        request -= allowed_papers[i]
-        balance -= allowed_papers[i]
+        allowed_papers = [100, 50, 20, 10, 5, 2, 1] # Allowed papers and piece of money in Tunisia
+        for i in range(len(allowed_papers)):
+            while request >= allowed_papers[i]:
+                print("Give -> %3i DNT" %(allowed_papers[i]))
+                request -= allowed_papers[i]
+                balance -= allowed_papers[i]
+        return balance
+    except ValueError as exp:
+        return exp
 
-print("***********Balance***********")
-print("Your balance is now %3i DNT" %(balance))
-print("*****************************")
+balance = withdraw(balance, request)
+
+if balance is 'Invalid Amount':
+    print("You don't have this amount of money in your account!")    
+else:
+    print("***********Balance***********")
+    print("Your balance is now %3i DNT" %(balance))
+    print("*****************************")
+    
+    
         
        
            
